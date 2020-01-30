e10da48ba79aa1e39d9914faed624df3a74ff1fb
Kalyani <kalyanirane19@gmail.com>
2020-01-30 17:05:55 +0530

    ADDED : Function to check if it's noPlay,Ladder,Snake

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -7,4 +7,21 @@ playerPosition=0
 
 #Get a number of die
 dieNumber=$((RANDOM % 6 + 1))
-echo "Die Result= $dieNumber"
+
+#Function to check the next step of player
+function checkNextStep()
+{
+	case $((RANDOM % 3)) in
+			0)playerPosition=$playerPosition
+				;;
+			1)playerPosition=$((playerPosition + dieNumber))
+				;;
+			2)playerPosition=$((playerPosition - dieNumber))
+				;;
+	esac
+}
+
+#function calling
+checkNextStep
+
+echo "Player current position= $playerPosition"
