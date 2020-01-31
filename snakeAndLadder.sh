315ca32e5d7166db8919454e1450a0fde4101260
kalyaniRane <kalyanirane19@gmail.com>
2020-01-31 18:16:14 +0530

    ADDED : Check The Players Exact Winning Position is 100 or Not

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,22 +1,28 @@
-#!/bin/bash
+ #!/bin/bash
 
 echo "Welcome snake And Ladder problem"
+
+#Initialize the constant
+NO_PLAY=0
+LADDER=1
+SNAKE=2
 
 #Initialize Variable
 playerPosition=0
 
-#Get a number of die
-dieNumber=$((RANDOM % 6 + 1))
-
 #Function to check the next step of player
 function checkNextStep()
 {
+	#Get a number of die
+	dieNumber=$((RANDOM % 6 + 1))
+	
+	#Get random number and check the next step for player
 	case $((RANDOM % 3)) in
-			0)playerPosition=$playerPosition
+			$NO_PLAY)playerPosition=$playerPosition
 				;;
-			1)playerPosition=$((playerPosition + dieNumber))
+			$LADDER)playerPosition=$((playerPosition + dieNumber))
 				;;
-			2)playerPosition=$((playerPosition - dieNumber))
+			$SNAKE)playerPosition=$((playerPosition - dieNumber))
 				;;
 	esac
 }
@@ -24,12 +30,16 @@ function checkNextStep()
 #Function to check players position
 function checkPlayersPosition()
 {
-	while [ $playerPosition -le 100 ]
+	while [ $playerPosition -ne 100 ]
 	do
 			checkNextStep
+
 			if [[ $playerPosition -lt 0 ]]
 			then
 					playerPosition=0
+			elif [[ $playerPosition -gt 100 ]]
+			then
+					playerPosition=$((playerPosition - dieNumber))
 			fi
 		echo "Player current position= $playerPosition"
 	done
